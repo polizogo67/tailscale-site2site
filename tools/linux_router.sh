810d8a108b18be067d5bae4189fcc32bd2dde6a1
polizogo <nate.polizogo@gmail.com>
2024-09-30 21:51:18 +0300

    Update linux_router.sh

--- tools/linux_router.sh
+++ tools/linux_router.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# # Install Tailscale
-# curl -sSL https://tailscale.com/install.sh | sh
 
-# # Enanble IP forwarding
-# echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-# echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
-# sudo sysctl -p /etc/sysctl.conf
+# Install Tailscale
+curl -sSL https://tailscale.com/install.sh | sh
 
 # Check if CIDR argument is provided
 if [ -z "$1" ]; then
@@ -14,6 +10,17 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo "Advertising: $1"
+
+# Enanble IP forwarding
+# echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+# echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+# sudo sysctl -p /etc/sysctl.conf
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+
+
 # Start Tailscale with advertise-routes
-# tailscale up --advertise-routes=$1 --snat-subnet-routes=false --accept-routes
-exho $1
+tailscale up --advertise-routes=$1 --snat-subnet-routes=false --accept-routes
