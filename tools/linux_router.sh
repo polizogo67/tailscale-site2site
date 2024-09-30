02662965bbab2b9b05b294778ff32423c152c820
polizogo <nate.polizogo@gmail.com>
2024-09-30 21:55:26 +0300

    Update linux_router.sh

--- tools/linux_router.sh
+++ tools/linux_router.sh
@@ -16,6 +16,7 @@ echo "Advertising: $1"
 # echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 # echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
 # sudo sysctl -p /etc/sysctl.conf
+sudo rm -rf /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
@@ -23,4 +24,4 @@ sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
 
 
 # Start Tailscale with advertise-routes
-tailscale up --advertise-routes=$1 --snat-subnet-routes=false --accept-routes
+sudo tailscale up --advertise-routes=$1 --snat-subnet-routes=false --accept-routes
